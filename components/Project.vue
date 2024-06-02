4b2c32faf39ad780259e97b97ca2ba4fd97b245b
BingFengHung <joe94008@gmail.com>
2024-06-02 09:58:14 +0800

    <feat>: 調整 Project 與 SideProject 卡片樣式

--- components/Project.vue
+++ components/Project.vue
@@ -1,9 +1,11 @@
 <template>
-  <div> 
-    <div> {{ title }} </div>
-    <img :src="imageUrl"/>  
-    <div> {{ contents }} </div>
-    <span v-for="(important, index) in importants" :key="index">{{ important }}</span>
+  <div class="card"> 
+    <div class="card_title"> {{ title }} </div>
+    <img class="card_img" :src="imageUrl"/>  
+    <div class="card_content"> {{ contents }} </div>
+    <div class="card_tag"> 
+      <span v-for="(important, index) in importants" :key="index">{{ important }}</span>
+    </div>
   </div>
 </template>
 <script setup>
@@ -24,6 +26,37 @@
 </script>
 
 <style scoped>
+.card {
+  padding: 5px;
+  background-color: white;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, .2);
+}
+
+.card_title {
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+
+.card_content {
+  margin: 0px 5px;
+}
+
+.card_img {
+  width: 240px;
+  height: 160px;
+  margin: 10px 0px;
+}
+
+.card_tag {
+  margin: 10px 0px;
+  align-self: self-start;
+}
+
 span {
   display: inline-block;
   border: 1px solid #b4be13;
--- components/SideProject.vue
+++ components/SideProject.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <div>{{ title }}</div>
-    <div>{{ contents }}</div>
-    <img :src="imageUrl"/>
+  <div class="card">
+    <div class="card_title">{{ title }}</div>
+    <img class="card_img" :src="imageUrl"/>
+    <div class="card_content">{{ contents }}</div>
   </div>
 </template>
 
@@ -15,4 +15,30 @@
 </script>
 
 <style scoped>
+.card {
+  padding: 5px;
+  background-color: white;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, .2);
+}
+
+.card_title {
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+    
+.card_img {
+  width: 240px;
+  height: 160px;
+  margin: 10px 0px;
+}
+    
+.card_content {
+  margin: 0px 5px;
+}
+
 </style>
